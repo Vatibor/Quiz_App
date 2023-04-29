72c0d172ef436ae37ac23a21b0636eb30f48596e
Varga Attila Tibor <h053414@stud.u-szeged.hu>
2023-04-29 19:55:27 +0200

    # Make users.php modify function frontend with modal

--- users.php
+++ users.php
@@ -11,6 +11,7 @@ include_once('functions.php');
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
+  <script src="jquery-3.6.4.min.js"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   <link rel="stylesheet" href="style_users.css">
@@ -29,7 +30,7 @@ include_once('functions.php');
           <tr>
             <th colspan="3">Username</th>
           </tr>
-          
+
         </thead>
         <tbody>
           <?php
@@ -39,11 +40,11 @@ include_once('functions.php');
           while ($oneRow = oci_fetch_array($users, OCI_ASSOC + OCI_RETURN_NULLS)) {
             echo '<tr>';
             echo '<td class="align-middle">' . $oneRow["NEV"] . '</td>';
-            echo '<td class="w-10"><button type="button" class="btn btn-warning">Modify</button></td>';
+            echo '<td class="w-10"><button type="button" class="btn btn-warning" data-bs-toggle="modal" data-id="' . $oneRow["NEV"] . '" data-bs-target="#exampleModal">Modify</button></td>';
             echo '
               <td class="w-10">
                 <form method="POST" action=deleteuser.php>
-                  <input type="hidden" name="id" value="'.$oneRow["FID"].'" />
+                  <input type="hidden" name="id" value="' . $oneRow["FID"] . '" />
                   <input type="submit" class="btn btn-danger" value="Delete"></input>
                 </form>
               
@@ -57,11 +58,37 @@ include_once('functions.php');
       </table>
       <php? ?>
     </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <input type="text" name="userName" id="userName" value=""/>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
     crossorigin="anonymous"></script>
- 
+
+  <script>
+    $(document).on("click", ".btn-warning", function () {
+      var myUserName = $(this).data('id');
+      $(".modal-body #userName").val(myUserName);
+    });
+  </script>
+
 </body>
 
 </html>
